49a7939c6deb3a739eed1f78ecc87182bf9ff52e
MSterben <give2052@gmail.com>
2022-04-25 23:50:15 +0300

    hald part of leasson 8

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const app = Vue.createApp({
     data(){
         return {
+            brand:'Vue Mastery',
             cart: 0,
             product: 'Socks',
             discription: ' create by bavovna',
             image: '/assest/images/socks_blue.jpg',
-            url: 'https://www.google.com',
-            inStock: false,
+            url: 'https://www.google.com', 
             inventory: 0,
             onSale: true,
+            selectedVariant: 0,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-                {id: 2234, color:'green',image: '/assest/images/socks_green.jpg',},
-                {id: 2235, color:'blue', image: '/assest/images/socks_blue.jpg'}
+                {id: 2234, color:'green',image: '/assest/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color:'blue', image: '/assest/images/socks_blue.jpg', quantity: 0}
             ],
             sizes:['24','26','27']
             
@@ -25,8 +26,19 @@ const app = Vue.createApp({
         minusToCart(){
             this.cart -=1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
         }
-    }  
+    },  
+    computed:{
+        title(){
+            return this.brand + " " + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        }
+    }
 }) 
