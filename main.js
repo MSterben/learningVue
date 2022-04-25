07eed6d1fc0374beabcc715802c0834f2672aad9
MSterben <give2052@gmail.com>
2022-04-25 23:08:16 +0300

    end of  leasson 6

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data(){
         return {
+            cart: 0,
             product: 'Socks',
             discription: ' create by bavovna',
             image: '/assest/images/socks_blue.jpg',
@@ -10,11 +11,22 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-                {id: 2234, color:'green'},
-                {id: 2235, color:'blue'}
+                {id: 2234, color:'green',image: '/assest/images/socks_green.jpg',},
+                {id: 2235, color:'blue', image: '/assest/images/socks_blue.jpg'}
             ],
             sizes:['24','26','27']
             
         }
-    }    
+    },  
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        minusToCart(){
+            this.cart -=1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+    }  
 }) 
