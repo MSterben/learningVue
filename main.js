3fa32be0456d0cf9a7f683623bce5b7dd52254a7
MSterben <give2052@gmail.com>
2022-04-25 22:47:50 +0300

    end of leasson 5

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = Vue.createApp({
             url: 'https://www.google.com',
             inStock: false,
             inventory: 0,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton','30% wool','20% polyester'],
+            variants: [
+                {id: 2234, color:'green'},
+                {id: 2235, color:'blue'}
+            ],
+            sizes:['24','26','27']
             
         }
     }    
