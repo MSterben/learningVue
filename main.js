be62eb9c49b24bdfcf53e50789f9a80f8d5b3c76
MSterben <give2052@gmail.com>
2022-04-26 20:46:57 +0300

    end of leasson 9

--- main.js
+++ main.js
@@ -1,50 +1,9 @@
 const app = Vue.createApp({
     data(){
         return {
-            onSale:  true,
-            brand:'Vue Mastery',
             cart: 0,
-            product: 'Socks',
-            discription: ' create by bavovna',
-            url: 'https://www.google.com', 
-            inventory: 0,
-            onSale: true,
-            selectedVariant: 0,
-            details: ['50% cotton','30% wool','20% polyester'],
-            variants: [
-                {id: 2234, color:'green',image: '/assest/images/socks_green.jpg', quantity: 50},
-                {id: 2235, color:'blue', image: '/assest/images/socks_blue.jpg', quantity: 0}
-            ],
-            sizes:['24','26','27']
-            
+            premium: true,
         }
     },  
-    methods:{
-        addToCart(){
-            this.cart +=1
-        },
-        minusToCart(){
-            this.cart -=1
-        },
-        updateVariant(index){
-            this.selectedVariant = index
-        }
-    },  
-    computed:{
-        title(){
-            return this.brand + " " + this.product
-        },
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity
-        },
-        writeOnSale(){
-            if(this.onSale){
-            return this.brand + " " + this.product+ " is on sale" 
-            }
-            return ""
-        }
-    }
+    methods: {}
 }) 
